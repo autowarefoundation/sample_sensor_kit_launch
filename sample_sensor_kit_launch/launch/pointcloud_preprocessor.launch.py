a719ef210d5d238ab24cc422de262b19af5390e9
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2021-11-18 18:33:40 +0900

    remove unused pointcloud preprocessor components (#2)

--- sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -15,17 +15,14 @@
 
 import launch
 from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
 from launch.actions import OpaqueFunction
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
-from launch_ros.substitutions import FindPackageShare
 import yaml
 
 
@@ -94,89 +91,13 @@ def launch_setup(context, *args, **kwargs):
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
-    # set crop box filter as a component
-    cropbox_component = ComposableNode(
-        package=pkg,
-        plugin="pointcloud_preprocessor::CropBoxFilterComponent",
-        name="crop_box_filter",
-        remappings=[
-            ("input", "concatenated/pointcloud"),
-            ("output", "measurement_range_cropped/pointcloud"),
-        ],
-        parameters=[
-            {
-                "input_frame": LaunchConfiguration("base_frame"),
-                "output_frame": LaunchConfiguration("base_frame"),
-                "min_x": -50.0,
-                "max_x": 100.0,
-                "min_y": -50.0,
-                "max_y": 50.0,
-                "min_z": vehicle_info["min_height_offset"],
-                "max_z": vehicle_info["max_height_offset"],
-                "negative": False,
-            }
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
-
-    ground_component = ComposableNode(
-        package=pkg,
-        plugin="pointcloud_preprocessor::ScanGroundFilterComponent",
-        name="scan_ground_filter",
-        remappings=[
-            ("input", "measurement_range_cropped/pointcloud"),
-            ("output", "no_ground/oneshot/pointcloud"),
-        ],
-        parameters=[
-            {
-                "global_slope_max_angle_deg": 10.0,
-                "local_slope_max_angle_deg": 30.0,
-                "split_points_distance_tolerance": 0.2,
-                "split_height_distance": 0.2,
-            }
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
-
-    load_laserscan_to_occupancy_grid_map = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                FindPackageShare("laserscan_to_occupancy_grid_map"),
-                "/launch/laserscan_to_occupancy_grid_map.launch.py",
-            ]
-        ),
-        launch_arguments={
-            "container": "/sensing/lidar/pointcloud_preprocessor/pointcloud_preprocessor_container",
-            "input/obstacle_pointcloud": "no_ground/oneshot/pointcloud",
-            "input/raw_pointcloud": "concatenated/pointcloud",
-            "output": "occupancy_grid",
-            "use_intra_process": LaunchConfiguration("use_intra_process"),
-        }.items(),
-    )
-
-    occupancy_grid_map_outlier_component = ComposableNode(
-        package=pkg,
-        plugin="pointcloud_preprocessor::OccupancyGridMapOutlierFilterComponent",
-        name="occupancy_grid_map_outlier_filter",
-        remappings=[
-            ("~/input/occupancy_grid_map", "occupancy_grid"),
-            ("~/input/pointcloud", "no_ground/oneshot/pointcloud"),
-            ("~/output/pointcloud", "no_ground/pointcloud"),
-        ],
-        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
-    )
-
     # set container to run all required components in the same process
     container = ComposableNodeContainer(
         name="pointcloud_preprocessor_container",
         namespace="pointcloud_preprocessor",
         package="rclcpp_components",
         executable=LaunchConfiguration("container_executable"),
-        composable_node_descriptions=[
-            cropbox_component,
-            ground_component,
-            occupancy_grid_map_outlier_component,
-        ],
+        composable_node_descriptions=[],
         output="screen",
     )
 
@@ -193,7 +114,7 @@ def launch_setup(context, *args, **kwargs):
         condition=UnlessCondition(LaunchConfiguration("use_concat_filter")),
     )
 
-    return [container, concat_loader, passthrough_loader, load_laserscan_to_occupancy_grid_map]
+    return [container, concat_loader, passthrough_loader]
 
 
 def generate_launch_description():
