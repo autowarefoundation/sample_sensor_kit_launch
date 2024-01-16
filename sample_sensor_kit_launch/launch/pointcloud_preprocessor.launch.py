eb509250fb2035a24dbb52b1ccd519519b7cce36
kminoda <koji.minoda@tier4.jp>
2024-01-16 22:15:49 +0900

    fix: now works
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -64,9 +64,7 @@ def launch_setup(context, *args, **kwargs):
 
     target_container = (
         container
-        if UnlessCondition(
-            LaunchConfiguration("include_concat_node_in_pointcloud_container")
-        ).evaluate(context)
+        if UnlessCondition(LaunchConfiguration("use_pointcloud_container")).evaluate(context)
         else LaunchConfiguration("pointcloud_container_name")
     )
 
@@ -89,7 +87,7 @@ def generate_launch_description():
     add_launch_arg("base_frame", "base_link")
     add_launch_arg("use_multithread", "False")
     add_launch_arg("use_intra_process", "False")
-    add_launch_arg("include_concat_node_in_pointcloud_container", "False")
+    add_launch_arg("use_pointcloud_container", "False")
     add_launch_arg("pointcloud_container_name", "pointcloud_container")
     add_launch_arg("individual_container_name", "concatenate_container")
 
