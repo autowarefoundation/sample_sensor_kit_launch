bfe226093a90cc470a59a58e12a7e9ef5e9bc3ec
Yoshi Ri <yoshiyoshidetteiu@gmail.com>
2024-03-04 17:07:59 +0900

    feat(common_sensor_launch, pointcloud_preprocessor): change pointcloud interface to synchronized pointclouds (#91)
    * feat: edn
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    
    * fix: suppress spell-check
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    
    * style(pre-commit): autofix
    
    * feat: interface change in sensing lidar pointcloud. use synchronized pointcloud as sensor interface
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    
    ---------
    
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -37,12 +37,13 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             {
                 "input_topics": [
-                    "/sensing/lidar/top/pointcloud",
-                    "/sensing/lidar/left/pointcloud",
-                    "/sensing/lidar/right/pointcloud",
+                    "/sensing/lidar/top/pointcloud_before_sync",
+                    "/sensing/lidar/left/pointcloud_before_sync",
+                    "/sensing/lidar/right/pointcloud_before_sync",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
                 "input_twist_topic_type": "twist",
+                "publish_synchronized_pointcloud": True,
             }
         ],
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
