070cb9c9a6a21f07c50b854f25c77ac973b4d205
vividf <yihsiang.fang@tier4.jp>
2024-06-27 12:30:08 +0900

    chore: fix spell error
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- common_sensor_launch/launch/nebula_node_container.launch.py
+++ common_sensor_launch/launch/nebula_node_container.launch.py
@@ -85,7 +85,7 @@ def launch_setup(context, *args, **kwargs):
         sensor_calib_fp
     ), "Sensor calib file under calibration/ was not found: {}".format(sensor_calib_fp)
 
-    # Pointcloud preprocesser parameters
+    # Pointcloud preprocessor parameters
     distortion_corrector_node_param = ParameterFile(
         param_file=LaunchConfiguration("distortion_correction_node_param_path").perform(context),
         allow_substs=True,
